10f0cd142602abd2c8a27adf09d0a804ac246b74
Quantum345 <w2424184@gmail.com>
2026-02-13 10:51:13 +0200

    Add declaration of RecordBook's class constructors, operators, setter and getters.

--- src/RecordBook.h
+++ src/RecordBook.h
@@ -8,6 +8,21 @@ class RecordBook :
 {
 private:
     unsigned int course;
-    std::vector<Subject> subjects;
+    Subject* subjects;
+    unsigned int subjectsAmount;
+public:
+    RecordBook();
+    RecordBook(unsigned int course);
+    RecordBook(const RecordBook& rb);
+    RecordBook(RecordBook&& rb);
+    ~RecordBook();
+
+    RecordBook& operator=(const RecordBook&);
+    RecordBook& operator=(RecordBook&&);
+
+    void setCourse(unsigned int course);
+    
+    unsigned int getCourse();
+    unsigned int getSubjectsAmount();
 };
 
