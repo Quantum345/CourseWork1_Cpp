ab3df818fd117973d03219a978753ed75f1475d0
Quantum345 <w2424184@gmail.com>
2026-02-20 10:06:12 +0200

    Add UdcCode class declaration

--- src/UdcCode.h
+++ src/UdcCode.h
@@ -1,4 +1,25 @@
 #pragma once
+#include <string>
+
 class UdcCode
-{};
+{
+private:
+    std::string code;
+public:
+    UdcCode();
+    UdcCode(const std::string& code);
+    UdcCode(const UdcCode& udcCode);
+    UdcCode(UdcCode&& udcCode);
+    ~UdcCode();
+
+    UdcCode& operator=(const UdcCode& udcCode);
+    UdcCode& operator=(UdcCode&& udcCode);
+
+    void setUdcCode(const std::string& code);
+
+    std::string getUdcCode();
+
+    friend std::istream& operator>>(std::istream& is, UdcCode& udcCode);
+    friend std::ostream& operator<<(std::ostream& os, const UdcCode& udcCode);
+};
 
