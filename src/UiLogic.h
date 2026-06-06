845c27752472854d835711d1b7fddc535684826e
Quantum345 <w2424184@gmail.com>
2026-06-06 19:34:18 +0300

    Add helpers for UiLogic

--- src/UiLogic.h
+++ src/UiLogic.h
@@ -7,15 +7,22 @@ private:
     static MyContainer<SemesterBook> semesterBooks;
     static MyContainer<Book*> books;
 public:
+    static bool yesNoPrompt(const std::string& prompt, bool isYesDefault);
+    static bool isIndexValid(const std::string& message, size_t idx, size_t size);
+
     static void fileRead();
     static void fileWrite();
 
+    static bool semesterBooksIsEmpty();
     static void semesterBookViewList();
     static void semesterBookViewDetails();
     static void semesterBookEdit();
     static void semesterBookAdd();
     static void semesterBookRemove();
 
+    static bool booksIsEmpty();
+    static size_t getBooksCount();
+    static size_t getRecordBooksCount();
     static void bookViewListAll();
     static void bookViewListBooks();
     static void bookViewListRecordBooks();
